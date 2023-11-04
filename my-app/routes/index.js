38908b34cfd9bd26cf326d00a0b1adc211b81562
Joshua Friday <fridayjoshua952@gmail.com>
2023-11-04 21:37:51 +0100

    added "/about" route to the "/" route

--- my-app/routes/index.js
+++ my-app/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/about",async (req,res)=>{
+  res.send(`<h1>About Us Page</h1>`);
+})
+
 module.exports = router;
